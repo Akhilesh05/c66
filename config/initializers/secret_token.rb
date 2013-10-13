f2bbae097f73bfff445cd7898007d4da0987dc93
Akhilesh05 <akhile@dr.com>
2013-10-13 15:21:02 +0000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cloud66::Application.config.secret_key_base = 'b69e369c03bd7f5432f66b95d95dbcae750353e4e48929019938cf181d4cedd99ad17f898663158e4c0d41a10080dfeb04cde3cad30b57c3f9833c4196002cca'
